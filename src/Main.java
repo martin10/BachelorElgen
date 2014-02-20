59f753d5952f74167ef1e25ec43bfde55911fa54
3catj3 <3catj3@gmail.com>
2014-02-20 14:32:27 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -10,10 +10,11 @@ import org.eclipse.swt.events.SelectionAdapter;
 import org.eclipse.swt.events.SelectionEvent;
 import org.eclipse.swt.widgets.Menu;
 import org.eclipse.swt.widgets.MenuItem;
+import org.eclipse.swt.custom.CLabel;
 
 
 public class Main {
-
+//test
 	protected Shell shlAnalyseverktyForElgdata;
 	private Text text;
 
@@ -70,17 +71,92 @@ public class Main {
 		Menu menu = new Menu(shlAnalyseverktyForElgdata, SWT.BAR);
 		shlAnalyseverktyForElgdata.setMenuBar(menu);
 		
+		MenuItem mntmHeim = new MenuItem(menu, SWT.CASCADE);
+		mntmHeim.setText("Heim");
+		
+		Menu menu_2 = new Menu(mntmHeim);
+		mntmHeim.setMenu(menu_2);
+		
+		MenuItem mntmImport = new MenuItem(menu, SWT.CASCADE);
+		mntmImport.setText("Import");
+		
+		Menu menu_3 = new Menu(mntmImport);
+		mntmImport.setMenu(menu_3);
+		
+		MenuItem mntmExcel = new MenuItem(menu_3, SWT.NONE);
+		mntmExcel.setText("Excel");
+		
+		MenuItem mntmExport = new MenuItem(menu, SWT.CASCADE);
+		mntmExport.setText("Export");
+		
+		Menu menu_4 = new Menu(mntmExport);
+		mntmExport.setMenu(menu_4);
+		
+		MenuItem mntmExcel_1 = new MenuItem(menu_4, SWT.NONE);
+		mntmExcel_1.setText("Excel");
+		
+		MenuItem mntmPublisering = new MenuItem(menu_4, SWT.NONE);
+		mntmPublisering.setText("Publisering");
+		
+		MenuItem mntmStatistikk = new MenuItem(menu, SWT.CASCADE);
+		mntmStatistikk.setText("Statistikk");
+		
+		Menu menu_5 = new Menu(mntmStatistikk);
+		mntmStatistikk.setMenu(menu_5);
+		
+		MenuItem mntmGrafer = new MenuItem(menu_5, SWT.NONE);
+		mntmGrafer.setText("Grafer");
+		
+		MenuItem mntmMultivariatAnalyse = new MenuItem(menu_5, SWT.NONE);
+		mntmMultivariatAnalyse.setText("Multivariat analyse");
+		
+		MenuItem mntmBivariatAnalyse = new MenuItem(menu_5, SWT.NONE);
+		mntmBivariatAnalyse.setText("Bivariat analyse");
+		
+		MenuItem mntmUnivariatAnalyse = new MenuItem(menu_5, SWT.NONE);
+		mntmUnivariatAnalyse.setText("Univariat analyse");
+		
 		MenuItem mntmRegistrering = new MenuItem(menu, SWT.CASCADE);
-		mntmRegistrering.setText("registrering");
+		mntmRegistrering.setText("Registrering");
 		
-		Menu menu_1 = new Menu(mntmRegistrering);
-		mntmRegistrering.setMenu(menu_1);
+		Menu menu_6 = new Menu(mntmRegistrering);
+		mntmRegistrering.setMenu(menu_6);
 		
-		MenuItem mntmTannanalyse = new MenuItem(menu_1, SWT.NONE);
-		mntmTannanalyse.setText("tannanalyse");
+		MenuItem mntmSettElg = new MenuItem(menu_6, SWT.NONE);
+		mntmSettElg.setText("Sett elg");
 		
-		MenuItem mntmVrdata = new MenuItem(menu_1, SWT.NONE);
-		mntmVrdata.setText("v\u00E6rdata");
+		MenuItem mntmTannanalyse = new MenuItem(menu_6, SWT.NONE);
+		mntmTannanalyse.setText("Tannanalyse");
+		
+		MenuItem mntmVrdata = new MenuItem(menu_6, SWT.NONE);
+		mntmVrdata.setText("V\u00E6rdata ");
+		
+		MenuItem mntmSkogsanalyse = new MenuItem(menu_6, SWT.NONE);
+		mntmSkogsanalyse.setText("Skogsanalyse");
+		
+		MenuItem mntmBeiteregistrering = new MenuItem(menu_6, SWT.NONE);
+		mntmBeiteregistrering.setText("Beiteregistrering");
+		
+		MenuItem mntmArkiv = new MenuItem(menu, SWT.CASCADE);
+		mntmArkiv.setText("Arkiv");
+		
+		Menu menu_7 = new Menu(mntmArkiv);
+		mntmArkiv.setMenu(menu_7);
+		
+		Menu menu_1 = new Menu(shlAnalyseverktyForElgdata);
+		shlAnalyseverktyForElgdata.setMenu(menu_1);
+		
+		Button button = new Button(shlAnalyseverktyForElgdata, SWT.NONE);
+		button.setBounds(325, -24, 75, 25);
+		button.setText("?");
+		
+		Button btnNewButton = new Button(shlAnalyseverktyForElgdata, SWT.NONE);
+		btnNewButton.setBounds(342, -24, 94, 25);
+		btnNewButton.setText("New Button");
+		
+		Button btnHjelp = new Button(shlAnalyseverktyForElgdata, SWT.NONE);
+		btnHjelp.setBounds(344, -24, 75, 25);
+		btnHjelp.setText("HJELP?");
 		
 		
 
